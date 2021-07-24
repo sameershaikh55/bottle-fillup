fe99cb3c6db15bd46044e5438d8834c9e1bc50ed
sameer-shaikh <sameermubeen4@gmail.com>
2021-07-25 00:46:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -36,6 +36,13 @@ function App() {
 		10;
 	// CONVERTING VALUE INTO NUMBER END
 
+	const message =
+		(convertedNum === 10 && <p>Red beer - 10$</p>) ||
+		(convertedNum === 20 && <p>Blue beer - 20$</p>) ||
+		(convertedNum === 30 && <p>Green beer - 30$</p>) ||
+		(convertedNum === 40 && <p>Yellow beer - 40$</p>) ||
+		(convertedNum === 50 && <p>Jas beer - 50$</p>);
+
 	return (
 		<div className="page_container">
 			{/* FILL COLOR START */}
@@ -58,38 +65,52 @@ function App() {
 
 			<div className="content_container">
 				{/* CONTENT START */}
-				<h1 className="text-center fw-bolder">How much beere do you want?</h1>
+				<h1 className="text-center fw-bolder mb-0">
+					How much beere do you want?
+				</h1>
 				<br />
 				{/* CONTENT END */}
-				
+
+				<div className="text-center h4 text-decoration-underline mb-5 fw-bold">
+					{message}
+				</div>
+
 				{/* SLIDER START */}
 				<SliderC slideChange={slideChange} inpField={inpField} />
 				{/* SLIDER END */}
+			</div>
 
-				{/* INPUT START */}
-				<input
-					style={{
-						border: `${
-							(convertedNum > 50 && "2px solid red") ||
-							(convertedNum < 10 && "2px solid red") ||
-							"none"
-						}`,
-					}}
-					type="text"
-					value={inpField}
-					onChange={handleChange}
-					placeholder="Enter Price"
-				/>
-				{/* INPUT START */}
-
-				{/* ERRORS START */}
-				{convertedNum > 50 && (
-					<p className="error">please enter the price less than 50</p>
-				)}
-				{convertedNum < 10 && (
-					<p className="error">please enter the price more than 10</p>
-				)}
-				{/* ERRORS END */}
+			<div className="input_container">
+				<div className="inner_inp_container d-flex align-items-center justify-content-between">
+					<div className="text-white mb-0">Place your order</div>
+					{/* INPUT START */}
+					<input
+						style={{
+							border: `${
+								(convertedNum > 50 && "2px solid red") ||
+								(convertedNum < 10 && "2px solid red") ||
+								"none"
+							}`,
+						}}
+						type="number"
+						value={inpField}
+						onChange={handleChange}
+						placeholder="0"
+						min="10"
+						max="50"
+					/>
+					{/* INPUT START */}
+				</div>
+				<div className="errors_container">
+					{/* ERRORS START */}
+					{convertedNum > 50 && (
+						<p className="error">Please enter the price less than 50$</p>
+					)}
+					{convertedNum < 10 && (
+						<p className="error">Please enter the price more than 10$</p>
+					)}
+					{/* ERRORS END */}
+				</div>
 			</div>
 
 			{/* CLOUD START */}
